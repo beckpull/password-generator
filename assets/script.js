6dd5df778d8a28d8a244b6a2f611137775252daf
Rebecca Feltman <153051811+refeltman83@users.noreply.github.com>
2024-01-09 15:54:32 -0700

    debugging so no errors remain logged in console

--- assets/script.js
+++ assets/script.js
@@ -64,7 +64,7 @@ function generatePassword() {
 }
 
 
-// STARTER CODE:
+// STARTER CODE: I commented some out to prevent errors in the console; can revise if needed?
 
 // Get references to the #generate element
 // var generateBtn = document.querySelector("#generate");
@@ -79,4 +79,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
